84b3d1291c6b35eb35f804bc02f6e52799851528
Jerome Jia <iusr.jar@gmail.com>
2019-07-23 18:07:29 -0700

    initial commit to port to 2019.2 RC, functions may be broken though

--- src/main/java/com/mulgasoft/emacsplus/actions/search/ISearchBackward.java
+++ src/main/java/com/mulgasoft/emacsplus/actions/search/ISearchBackward.java
@@ -1,34 +1,62 @@
 package com.mulgasoft.emacsplus.actions.search;
 
 import com.intellij.find.FindUtil;
-import com.intellij.ide.actions.SearchBackAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
 import com.mulgasoft.emacsplus.util.ActionUtil;
 import com.mulgasoft.emacsplus.util.EmacsIds;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ISearchBackward extends SearchBackAction {
-  @Override
-  public void actionPerformed(AnActionEvent e) {
-    delegateAction(e);
+import static com.intellij.openapi.editor.actions.IncrementalFindAction.SEARCH_DISABLED;
+
+public class ISearchBackward extends EditorAction implements DumbAware {
+  public ISearchBackward() {
+    this(false);
   }
 
-  protected static ISearch delegateAction(AnActionEvent e) {
-    Editor editor = FileEditorManager.getInstance(e.getProject()).getSelectedTextEditor();
-    ISearch searcher = ISearch.from(editor);
-    if (searcher == null) {
-      ActionUtil.dispatch(EmacsIds.ISEARCH_ID, e.getDataContext());
-      searcher = ISearch.from(editor);
-    } else {
-      searcher.requestFocus();
-      FindUtil.configureFindModel(false, editor, searcher.getFindModel(), false);
-    }
-
-    if (searcher != null) {
-      searcher.searchBackward();
-    }
-
-    return searcher;
+  public ISearchBackward(final boolean defaultRegexp) {
+    super(null);
+    setEnabledInModalContext(true);
+    this.setupHandler(new Handler());
+    this.defaultRegexp = defaultRegexp;
   }
+
+  private class Handler extends EditorActionHandler {
+    @Override
+    protected void doExecute(@NotNull Editor editor, @Nullable Caret caret, DataContext dataContext) {
+      ISearch searcher = ISearch.from(editor);
+      if (searcher == null) {
+        ActionUtil.dispatch(EmacsIds.ISEARCH_ID, dataContext);
+        searcher = ISearch.from(editor);
+      } else {
+        searcher.requestFocus();
+        FindUtil.configureFindModel(false, editor, searcher.getFindModel(), false);
+      }
+
+      if (searcher != null) {
+        if (defaultRegexp) {
+          searcher.getFindModel().setRegularExpressions(true);
+        }
+        searcher.searchBackward();
+      }
+    }
+
+    @Override
+    protected boolean isEnabledForCaret(@NotNull Editor editor, @NotNull Caret caret, DataContext dataContext) {
+      Project project = dataContext.getData(CommonDataKeys.PROJECT);
+      if (project == null) {
+        return false;
+      }
+      return !editor.isOneLineMode() && !SEARCH_DISABLED.get(editor, false);
+    }
+  }
+
+  private final boolean defaultRegexp; // TODO should this change?
 }
--- src/main/java/com/mulgasoft/emacsplus/actions/search/ISearchBackwardRegexp.java
+++ src/main/java/com/mulgasoft/emacsplus/actions/search/ISearchBackwardRegexp.java
@@ -3,12 +3,7 @@ package com.mulgasoft.emacsplus.actions.search;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
 public class ISearchBackwardRegexp extends ISearchBackward {
-  @Override
-  public void actionPerformed(AnActionEvent e) {
-    ISearch searcher = delegateAction(e);
-    if (searcher != null) {
-      searcher.getFindModel().setRegularExpressions(true);
-    }
-
+  public ISearchBackwardRegexp() {
+    super(true);
   }
 }
